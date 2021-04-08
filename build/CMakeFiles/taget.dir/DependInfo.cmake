58cebf7fe345f874030829d55c66eea93378a0b8
GAW_u_ooo <63202524+Yesterdaylove@users.noreply.github.com>
2021-04-08 21:49:03 +0800

    Add files via upload

---
+++ build/CMakeFiles/taget.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/item_vscode/DL/example-app/taget.cpp" "/home/item_vscode/DL/example-app/build/CMakeFiles/taget.dir/taget.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/python3.8"
+  "/home/pinjin/download/matplotlib-cpp"
+  "/usr/local/lib/python3.8/dist-packages/numpy/core/include"
+  "../taget"
+  "../PRIVATE"
+  "/home/pinjin/all_libtorch/libtorch_1.7.1/include"
+  "/home/pinjin/all_libtorch/libtorch_1.7.1/include/torch/csrc/api/include"
+  "/usr/local/cuda-11.1/include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/taget.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/taget.dir/taget.cpp.o"
+  "taget"
+  "taget.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/taget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
